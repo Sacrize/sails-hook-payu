1cd0cd017539756b9a5233e5186ccad061fb118d
Jakub Paprocki <jakub@codeland.pl>
2020-11-18 20:26:08 +0100

    v1.0.1

--- index.js
+++ index.js
@@ -14,12 +14,49 @@ module.exports = function (sails) {
                 key: '',
                 environment: 'production',
                 shopId: '',
+                payuWhitelistTest: [
+                    '185.68.14.10',
+                    '185.68.14.11',
+                    '185.68.14.12',
+                    '185.68.14.26',
+                    '185.68.14.27',
+                    '185.68.14.28',
+                ],
+                payuWhitelistProduction: [
+                    '185.68.12.10',
+                    '185.68.12.11',
+                    '185.68.12.12',
+                    '185.68.12.26',
+                    '185.68.12.27',
+                    '185.68.12.28',
+                ]
             }
         },
         initialize: function () {
             sails.log.info('Initializing hook (`sails-hook-payu`)');
             config = sails.config[this.configKey];
             _initPayu();
+        },
+        configure: function () {
+          sails.config[this.configKey].payuWhitelist = sails.config[this.configKey].environment === 'test' ? sails.config[this.configKey].payuWhitelistTest : sails.config[this.configKey].payuWhitelistProduction;
+        },
+        routes: {
+            before: {
+                '/cb/payu': function (req, res, next) {
+                    let headers = req.headers;
+                    let whitelist = config.payuWhitelist;
+                    if (whitelist.includes(req.ip)) {
+                        return;
+                    }
+                    if (_verifyNotification(req.body, headers)) {
+                        sails.hooks.events.emit('payu-notification', { notification: req.body, });
+                        sails.log.debug('Payu notification is valid', { body: req.body, });
+                    } else {
+                        sails.log.error(new Error('Payu notification is not valid'), { body: req.body, headers, });
+                    }
+                    res.ok();
+                }
+            }
         },
         getShopData: _getShopData,
         getOrder: _getOrder,
